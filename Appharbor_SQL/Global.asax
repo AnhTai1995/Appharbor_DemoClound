f0c43cf957254e4b967ee40743c59917d8512dd6
AnhTai1995 <anhtai.hcmus95@gmail.com>
2016-11-28 09:27:09 +0700

    Add project files.

---
+++ Appharbor_SQL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Appharbor_SQL.MvcApplication" Language="C#" %>
---
+++ Appharbor_SQL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Appharbor_SQL.Startup))]
+namespace Appharbor_SQL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
